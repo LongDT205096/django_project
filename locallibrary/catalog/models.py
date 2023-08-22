4df60a0efb2e733df40ef6de12acbc502c1ac3fb
LongDT205096 <dinhthanhlong0@gmail.com>
2023-08-22 08:52:30 +0700

    part_8

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,6 +1,8 @@
 from django.utils.translation import gettext
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 class Genre(models.Model):
@@ -43,13 +45,19 @@ class BookInstance(models.Model):
     due_back = models.DateField(null=True, blank=True)
     LOAN_STATUS = (('m', 'Maintenance'),('o', 'On loan'),('a', 'Available'),('r', 'Reserved'),)
     status = models.CharField(max_length=1,choices=LOAN_STATUS,blank=True,default='m',help_text=gettext('Book availability',))
-    
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
     class Meta:
         ordering = ['due_back']
+        permissions = (("can_mark_returned", "Set book as returned"),)
     
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+    
+    @property
+    def is_overdue(self):
+        return self.due_back and date.today() > self.due_back
 
 class Author(models.Model):
     """Model representing an author."""
--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:primary_key>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 ]
